20351c9c3d1445566651d2f0384c8e9fede84df0
er567 <er567@manmandeMacBook-Pro.local>
2018-06-01 00:47:33 +0800

    :rocket: updata

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    console.log()
+  },
+  onLoad: function () {
+    
+  },
+  methods: function(e) {
+    
+  }
+})
